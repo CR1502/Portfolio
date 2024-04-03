6821df012d70f11a00da7791d80971374aa974d9
Craig Lionel Roberts <80633545+CR1502@users.noreply.github.com>
2024-04-04 01:28:24 +0530

    Updated Resume link

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,7 +40,7 @@ const HeroSection = () => {
           <div>
             
             <Link
-              href="https://drive.google.com/file/d/1tg7H5vOoDWWWlncc8JkFfG7p8zD1TyfP/view?usp=sharing"
+              href="https://drive.google.com/file/d/1X2ygErICnNO3YmBexYtnB5YZHPc5y1kv/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
